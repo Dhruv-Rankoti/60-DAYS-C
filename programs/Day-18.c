5e65b5029a12c7ac1d7887f39a79f3a2e6dc3351
Dhruv-Rankoti <dhruvrankoti@gmail.com>
2024-03-30 16:24:52 +0530

    update day-18

---
+++ programs/Day-18.c
@@ -0,0 +1,22 @@
+// Write a Program to calculate factorial of a number.
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    unsigned long long int fact = 1;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    for(int i = 1; i <= num; i++){
+        fact *= i;
+    }
+    if(num >= 0){
+        printf("Factorial of %d is %d.", num, fact);
+    } else{
+        printf("Error! Factorial of a negative number doesn't exist.");
+    }
+    return 0;
+}
+
+// DAY-19 Problem
+// Write a Program to calculate primes between 1 to 100.
